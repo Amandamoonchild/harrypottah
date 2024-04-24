1df61d6a3a8038390fdb636d83b459e452bd6334
Amanda dos Santos Silva <123501998+Amandamoonchild@users.noreply.github.com>
2024-04-24 11:32:27 -0300

    feat: atualização da index

--- database/script.sql
+++ database/script.sql
@@ -10,4 +10,14 @@ CREATE TABLE bruxo (
     patrono VARCHAR(100) NOT NULL
 );
 
-INSERT INTO bruxo (nome, idade, casa, habilidade, sangue, patrono) VALUES ('');
+INSERT INTO bruxo (nome, idade, casa, habilidade, sangue, patrono) VALUES ('Amanda', '17', 'Lufa-lufa', 'Animagia', 'Mestiço', 'lontra');
+
+CREATE TABLE varinha (
+    id SERIAL PRIMARY KEY,
+    material VARCHAR(100) NOT NULL,
+    comprimento  INT,
+    nucleo VARCHAR(100) NOT NULL,
+    madeIn DATE NOT NULL
+);
+
+INSERT INTO bruxo (material, comprimento, nucleo, madeIn) VALUES ('abeto', '25', 'Pêlo de Unicórnio', '2004-03-03');
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Pool } = require('pg')
 
 const app = express();
-const port = 4000;
+const port = 4006;
 
 app.use(express.json());
 
@@ -14,118 +14,128 @@ const pool = new Pool({
     port: '5432',
 });
 
-function calcularIdade(datanascimento) {
-    const hoje = new Date();
-    let idade = hoje.getFullYear() - datanascimento.getFullYear();
-    const mesAtual = hoje.getMonth();
-    const mesNascimento = datanascimento.getMonth();
-    if (mesNascimento > mesAtual || (mesNascimento === mesAtual && hoje.getDate() < datanascimento.getDate())) {
-        idade--;
-    }
-    return idade;
-}
-
-function calcularSigno(mes, dia) {
-    if ((mes === 0 && dia >= 20) || (mes === 1 && dia <= 18)) {
-        return 'Aquário';
-    } else if ((mes === 1 && dia >= 19) || (mes === 2 && dia <= 20)) {
-        return 'Peixes';
-    } else if ((mes === 2 && dia >= 21) || (mes === 3 && dia <= 19)) {
-        return 'Áries';
-    } else if ((mes === 3 && dia >= 20) || (mes === 4 && dia <= 20)) {
-        return 'Touro';
-    } else if ((mes === 4 && dia >= 21) || (mes === 5 && dia <= 20)) {
-        return 'Gêmeos';
-    } else if ((mes === 5 && dia >= 21) || (mes === 6 && dia <= 22)) {
-        return 'Câncer';
-    } else if ((mes === 6 && dia >= 23) || (mes === 7 && dia <= 22)) {
-        return 'Leão';
-    } else if ((mes === 7 && dia >= 23) || (mes === 8 && dia <= 22)) {
-        return 'Virgem';
-    } else if ((mes === 8 && dia >= 23) || (mes === 9 && dia <= 22)) {
-        return 'Libra';
-    } else if ((mes === 9 && dia >= 23) || (mes === 10 && dia <= 22)) {
-        return 'Escorpião';
-    } else if ((mes === 10 && dia >= 23) || (mes === 11 && dia <= 21)) {
-        return 'Sagitário';
-    } else {
-        return 'Capricórnio'; 
-    }
-}
-
-app.get('/', (req, res) => {
-    res.send('Servidor funcionando 🎀')
-});
-
-app.get('/users', async (req, res) => {
+app.get('/bruxo', async (req, res) => {
     try {
-        const resultado = await pool.query('SELECT * FROM users');
+        const resultado = await pool.query('SELECT * FROM bruxo');
         res.json({
             total: resultado.rowCount,
-            users: resultado.rows
+            bruxo: resultado.rows
         });
     } catch (error) {
-        console.error('Erro ao obter todos os usuários 😲', error);
-        res.status(500).send({ mensagem: 'Erro ao obter todos os usuários 😲' })
+        console.error('Erro ao obter todos os bruxos 😲');
+        res.status(500).send({mensagem: 'Erro ao obter todos os bruxos 😲'})
     }
 })
 
-app.post('/users', async (req, res) => {
+app.post('/bruxo', async(req, res) => {
     try {
-        let { nome, email, nascimento } = req.body;
-
-        let datanascimento = new Date(nascimento);
-        let idade = calcularIdade(datanascimento);
-        let signo = calcularSigno(datanascimento.getMonth(), datanascimento.getDate());
-
-        await pool.query('INSERT INTO users (nome, email, nascimento, idade, signo) VALUES ($1, $2, $3, $4, $5)', [nome, email, datanascimento, idade, signo])
-        res.status(201).send({ mensagem: 'Sucesso ao criar usuario 😎' })
+        const {nome, email} = req.body;
+        await pool.query('INSERT INTO bruxos (nome, idade, casa, habilidade, sangue, patrono) VALUES ($1, $2, $3, $4, $5, $6)', [nome, idade, casa, habilidade, sangue, patrono])
+        res.status(201).send({mensagem: 'Sucesso ao criar bruxo 😎'})
     } catch (error) {
-        console.error('Erro ao criar o usuario 😲');
-        res.status(500).send({ mensagem: 'Erro ao criar o usuario 😲' })
+        console.error('Erro ao criar o bruxo 😲');
+        res.status(500).send({mensagem: 'Erro ao criar o bruxo 😲'})
     }
 })
 
-app.delete('/users/:id', async (req, res) => {
+app.delete('/bruxo/:id', async (req, res) => {
     try {
-        const { id } = req.params;
-        await pool.query('DELETE FROM users WHERE id = $1', [id]);
-        res.status(200).send({ mensagem: 'Sucesso ao deletar usuario 😎' })
+        const {id} = req.params;
+        await pool.query('DELETE FROM bruxo WHERE id = $1', [id]);
+        res.status(200).send({mensagem: 'Sucesso ao deletar bruxo 😎'})
     } catch (error) {
         console.error('Erro ao deletar o usuario 😲');
-        res.status(500).send({ mensagem: 'Erro ao deletar o usuario 😲' })
+        res.status(500).send({mensagem: 'Erro ao deletar o bruxo 😲'})
     }
 })
 
-app.put('/users/:id', async (req, res) => {
+app.put('/bruxo/:id', async(req, res) =>{
     try {
-        const { id } = req.params;
-        const { nome, email, nascimento } = req.body;
-        const datanascimento = new Date(nascimento);
-        const idade = calcularIdade(datanascimento);
-        const signo = calcularSigno(datanascimento.getMonth(), datanascimento.getDate());
-        await pool.query('UPDATE users SET nome = $1, email = $2, nascimento = $3, idade = $4, signo = $5 WHERE id = $6', [nome, email, datanascimento, idade, signo, id]);
-        res.status(200).send({ mensagem: 'Sucesso ao editar usuario 😎' })
+        const {id} = req.params;
+        const {nome, idade, casa, habilidade, sangue, patrono} = req.body;
+        await pool.query('UPDATE bruxo SET nome = $1, idade = $2, casa = $3, habilidade = $4, sangue = $5, patrono = $6, id = $7', [nome, idade, casa, habilidade, sangue, patrono, id]);
+        res.status(200).send({mensagem: 'Sucesso ao editar bruxo 😎'})
     } catch (error) {
         console.error('Erro ao editar o usuario 😲');
-        res.status(500).send({ mensagem: 'Erro ao editar o usuario 😲' })
+        res.status(500).send({mensagem: 'Erro ao editar o bruxo 😲'})
     }
 })
 
-app.get('/users/:id', async (req, res) => {
+app.get('/bruxo/:id', async(req, res) =>{
     try {
-        const { id } = req.params;
-        const result = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
-        if (result.rowCount === 0) {
-            res.status(404).send({ mensagem: 'Usuário não encontrado' });
-        } else {
-            res.json(result.rows[0]);
-        }
+        const {id} = req.params;
+        const {nome, idade, casa, habilidade, sangue, patrono} = req.body;
+        await pool.query('SELECT * FROM bruxo WHERE id = $1', [id]);
+        res.status(200).send({mensagem: 'Sucesso ao selecionar bruxo 😎'})
     } catch (error) {
-        console.error('Erro ao obter usuário por ID:', error);
-        res.status(500).send('Erro ao obter usuário por ID');
+        console.error('Erro ao selecionar o usuario 😲');
+        res.status(404).send({mensagem: 'Erro ao selecionar o bruxo 😲'})
     }
-});
+})
+
+
+////////Varinhas
+
+
+app.get('/varinha', async (req, res) => {
+    try {
+        const resultado = await pool.query('SELECT * FROM varinha');
+        res.json({
+            total: resultado.rowCount,
+            varinha: resultado.rows
+        });
+    } catch (error) {
+        console.error('Erro ao obter todos as varinhas 😲');
+        res.status(500).send({mensagem: 'Erro ao obter todos as varinhas 😲'})
+    }
+})
+
+app.post('/varinha', async(req, res) => {
+    try {
+        const {material, comprimento, nucleo, madeIn} = req.body;
+        await pool.query('INSERT INTO varinhas (material, comprimento, nucleo, madeIn) VALUES ($1, $2, $3, $4)', [material, comprimento, nucleo, madeIn])
+        res.status(201).send({mensagem: 'Sucesso ao criar varinhas 😎'})
+    } catch (error) {
+        console.error('Erro ao criar a varinha 😲');
+        res.status(500).send({mensagem: 'Erro ao criar a varinha 😲'})
+    }
+})
+
+app.delete('/varinha/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        await pool.query('DELETE FROM varinha WHERE id = $1', [id]);
+        res.status(200).send({mensagem: 'Sucesso ao deletar varinha 😎'})
+    } catch (error) {
+        console.error('Erro ao deletar a varinha 😲');
+        res.status(500).send({mensagem: 'Erro ao deletar a varinha 😲'})
+    }
+})
+
+app.put('/varinha/:id', async(req, res) =>{
+    try {
+        const {id} = req.params;
+        const {material, comprimento, nucleo, madeIn} = req.body;
+        await pool.query('UPDATE varinha SET material = $1, comprimento = $2, nucleo = $3, madeIn = $4', [material, comprimento, nucleo, madeIn]);
+        res.status(200).send({mensagem: 'Sucesso ao editar varinha 😎'})
+    } catch (error) {
+        console.error('Erro ao editar a varinha 😲');
+        res.status(500).send({mensagem: 'Erro ao editar varinha 😲'})
+    }
+})
+
+app.get('/varinha/:id', async(req, res) =>{
+    try {
+        const {id} = req.params;
+        const {material, comprimento, nucleo, madeIn} = req.body;
+        await pool.query('SELECT * FROM varinha WHERE id = $1', [id]);
+        res.status(200).send({mensagem: 'Sucesso ao selecionar varinha 😎'})
+    } catch (error) {
+        console.error('Erro ao selecionar a varinha 😲');
+        res.status(404).send({mensagem: 'Erro ao selecionar a varinha 😲'})
+    }
+})
+
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port} 🍄`)
